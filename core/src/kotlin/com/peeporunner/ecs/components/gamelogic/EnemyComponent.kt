9e90def9cc9c572a75cb763c0a5a492772f301e0
Maciej <gorski.maciej@o2.pl>
2021-08-19 12:49:37 +0200

    added enemy component class

---
+++ core/src/kotlin/com/peeporunner/ecs/components/gamelogic/EnemyComponent.kt
@@ -0,0 +1,10 @@
+package com.peeporunner.ecs.components.gamelogic
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool
+
+class EnemyComponent : Component, Pool.Poolable {
+    override fun reset() {
+
+    }
+}
